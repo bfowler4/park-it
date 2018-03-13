14275feedd56a5fc2ed7dfe2035f072ce5ee7e35
Brandon Fowler <bmfowler4@gmail.com>
2018-03-13 11:16:13 -1000

    updated README and added server base middleware

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
+const path = require(`path`);
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.get(`/`, (req, res) => {
-  return res.send(`server smoke test`);
+app.use(express.static(`public`));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(`*`, (req, res) => {
+  res.sendFile(path.join(__dirname, `..`, `public`, `index.html`));
 });
 
 app.listen(PORT, () => {
