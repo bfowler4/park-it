712af4fc5a7c7eaacab050fe4da97ee4424a99d0
Brandon Fowler <bmfowler4@gmail.com>
2018-03-14 13:14:18 -1000

    added postgis to database to query based on max distance from requested location

--- server/db/models/Space.js
+++ server/db/models/Space.js
@@ -3,6 +3,9 @@ const bookshelf = require(`./bookshelf`);
 class Space extends bookshelf.Model {
   get tableName() { return `spaces`; }
   get hasTimestamps() { return false; }
+  get geography() {
+    return { location: [`longitude`, `latitude`] };
+  }
 
   user() {
     return this.belongsTo(`User`);
