f0bf54f6f16124636298a78c2b963c3176200b7b
Bradley <brad@Bradleys-MacBook-Pro.local>
2018-03-15 12:13:08 -1000

    zoom in working

--- src/containers/ParkingHomePage.js
+++ src/containers/ParkingHomePage.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import Geocode from "react-geocode";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+
+
+
 const API_KEY = "AIzaSyCrACMzBiHlUg7YaKRFMww3BL7K8ym3QFI";
 Geocode.setApiKey(API_KEY);
 const google = window.google;
@@ -12,13 +17,16 @@ class HomePark extends Component {
 
     this.state = {
       search: ``,
-      predictions: []
+      predictions: [],
+      lat: 21.2969,
+      lng: -157.8171,
+      key:'',
     }
   }
 
   handleChange(event) {
     this.setState({ search: event.target.value });
-
+    
     if (event.target.value) {
       auto.getPlacePredictions({input: event.target.value}, (predictions, status) => {
         this.setState({ predictions: predictions });
@@ -27,27 +35,45 @@ class HomePark extends Component {
   }
 
   handleClick(event) {
-    this.setState({ search: event.target.innerHTML });
-    this.setState({ predictions: [] });
-  }
-
-  handleSubmit(e){
-    e.preventDefault();
-    Geocode.fromAddress(this.state.search).then(
+    console.log(event.target.innerHTML)
+    Geocode.fromAddress(event.target.innerHTML).then(
       response => {
         const { lat, lng } = response.results[0].geometry.location;
-        console.log(lat, lng);
+       
+        this.setState({ lat:lat  });
+        this.setState({ lng:lng  });
+        console.log(this.state.lng,this.state.lng);
+        this.setState({ key: Math.random() });
       },
       error => {
         console.error(error);
       }
     );
+
+
+
+
+
+    this.setState({ search: event.target.innerHTML });
+    this.setState({ predictions: [] });
+    
+    
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+   
   }
 
   render() {
+    const style = {
+      width: "400px",
+      height: "400px"
+    };
    
     return (
       <div className="App">
+        
         <input type="text" value={this.state.search} onChange={this.handleChange.bind(this)}/>
         {this.state.predictions.length > 0 && this.state.predictions.map((prediction, index) => {
           return <div 
@@ -59,9 +85,37 @@ class HomePark extends Component {
         <button 
         type="submit"
         onClick={this.handleSubmit.bind(this)}>Submit</button>
+          <br/> <br/> <br/> <br/>
+          <Map
+          key={this.state.key}
+          google={this.props.google}
+          style={style}
+          initialCenter={{
+            lat: this.state.lat,
+            lng: this.state.lng
+          }}
+          zoom={16}>
+
+          
+          
+          <Marker
+      title={'Title'}
+      name={'Parking location'}
+      position={{lat: this.state.lat, lng: this.state.lng}}
+      draggable={true}
+      />
+      
+
+          
+
+          </Map>
+
       </div>
     );
   }
 }
 
-export default HomePark;
+export default GoogleApiWrapper({
+  apiKey: (API_KEY)
+})(HomePark)
+
